6bf5a1b7b6643217e0d7c081bf92640f3aa02637
Fuad Farhan <fuadhoquefarhan05@gmail.com>
2025-12-30 11:59:33 -0500

    design update

--- script.js
+++ script.js
@@ -10,29 +10,3 @@ function createShootingStar() {
 }
 
 setInterval(createShootingStar, 500);
-
-// Floating PNG Images
-const images = [
-    'kodely.png',
-    'google.png',
-    'infosys.png'
-];
-
-function floatImages() {
-    const container = document.getElementById('floating-images');
-    images.forEach((src, i) => {
-        const img = document.createElement('img');
-        img.src = src;
-        img.style.top = Math.random() * window.innerHeight + 'px';
-        img.style.left = Math.random() * window.innerWidth + 'px';
-        container.appendChild(img);
-
-        // Animate movement
-        setInterval(() => {
-            img.style.top = Math.random() * window.innerHeight + 'px';
-            img.style.left = Math.random() * window.innerWidth + 'px';
-        }, 5000 + Math.random() * 5000);
-    });
-}
-
-floatImages();
